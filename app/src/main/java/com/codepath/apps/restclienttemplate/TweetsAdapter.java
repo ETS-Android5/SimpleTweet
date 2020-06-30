8dc66e1f46f9f2f2be6e1507f1bfd3cf007bb424
Mervyn Mathew <mmervyn@fb.com>
2020-06-30 16:40:26 -0400

    Made tweet show relative date on them

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
 
@@ -49,6 +53,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         TextView textViewName;
         TextView textViewScreenName;
         TextView textViewBody;
+        TextView textViewCreatedAt;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -56,14 +61,33 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             textViewName = itemView.findViewById(R.id.textViewName);
             textViewScreenName = itemView.findViewById(R.id.textViewScreenName);
             textViewBody = itemView.findViewById(R.id.textViewTweetBody);
+            textViewCreatedAt = itemView.findViewById(R.id.textViewCreatedAt);
         }
 
         public void bind(Tweet tweet) {
             textViewBody.setText(tweet.body);
             textViewName.setText(tweet.user.name);
             textViewScreenName.setText("@"+tweet.user.screenName);
+            textViewCreatedAt.setText(getRelativeTimeAgo(tweet.createdAt));
             Glide.with(context).load(tweet.user.profileImageUrl)
                                .into(imageViewProfileImage);
+        }
+
+        public String getRelativeTimeAgo(String rawJsonDate) {
+            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+            sf.setLenient(true);
+
+            String relativeDate = "";
+            try {
+                long dateMillis = sf.parse(rawJsonDate).getTime();
+                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_ALL).toString();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            return relativeDate;
         }
     }
 }
